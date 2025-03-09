7443618ee260b3847f0dd85e0acf7f5cf21ae9ee
Mahmor Mustapa <120547896+mustcoding@users.noreply.github.com>
2025-03-09 14:11:50 +0800

    remove baseURL

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -3,12 +3,6 @@ export default defineNuxtConfig({
   compatibilityDate: '2024-11-01',
   devtools: { enabled: true },
   modules: ['@nuxtjs/tailwindcss', 'shadcn-nuxt'],
-  app: {
-    baseURL: '/portfolio-mahmor/' // Must match your GitHub repository name
-  },
-  nitro: {
-    preset: 'static' // Ensures Nuxt generates a static site for GitHub Pages
-  },
   shadcn: {
     /**
      * Prefix for all the imported component

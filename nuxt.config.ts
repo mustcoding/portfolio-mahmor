67cc27851059de4388b5283670975ccf75afba0b
Mahmor Mustapa <120547896+mustcoding@users.noreply.github.com>
2025-03-09 17:29:33 +0800

    fix

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -3,15 +3,6 @@ export default defineNuxtConfig({
   compatibilityDate: '2024-11-01',
   devtools: { enabled: true },
   modules: ['@nuxtjs/tailwindcss', 'shadcn-nuxt'],
-  app: {
-    baseURL: '/portfolio-mahmor/', // Set your GitHub repo name
-  },
-  nitro: {
-    preset: 'static', // ✅ Ensure static site generation
-    prerender: {
-      routes: ['/'], // ✅ Ensure the homepage is generated
-    }
-  },
   shadcn: {
     /**
      * Prefix for all the imported component

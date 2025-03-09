3018b96655a65d54584c6d940e98d96c2757e7d7
Mahmor Mustapa <120547896+mustcoding@users.noreply.github.com>
2025-03-10 02:43:35 +0800

    fix avatar image

--- pages/mahmor-mustapa/index.vue
+++ pages/mahmor-mustapa/index.vue
@@ -12,7 +12,11 @@ const basePath = config.app.baseURL || "";
         <Avatar
           class="w-52 h-52 mt-10 md:w-96 md:h-96 drop-shadow-[0_0_10px_rgba(255,223,0,0.8)]"
         >
-          <AvatarImage :src="`${basePath}/image/mahmor.jpg`" alt="@unovue" />
+          <AvatarImage
+            :src="`${basePath}/image/mahmor.jpg`"
+            alt="@unovue"
+            class="object-cover rounded-full"
+          />
           <AvatarFallback>MM</AvatarFallback>
         </Avatar>
       </div>
